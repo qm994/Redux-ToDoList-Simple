fecd875187486088fb504f518c537056b0edfe6a
Qingyuan Ma <ma@MacBook-Pro-3.fios-router.home>
2020-04-11 17:39:13 -0400

    add files

---
+++ index.js
@@ -0,0 +1,62 @@
+function createStore (reducer) {
+    // The store should have four parts
+    // 1. The state
+    // 2. Get the state.
+    // 3. Listen to changes on the state.
+    // 4. Update the state
+  
+    let state;
+    let listeners = [];
+
+    const getState = () => state;
+    const subscribe = (listener) => {
+        listeners.push(listener);
+        return () => {
+            listeners = listeners.filter(l => l !== listener)
+        }
+    };
+
+    const dispatch = (action) => {
+        state = reducer(state, action);
+        listeners.forEach((listener) => listener());
+    }
+    
+  
+    return {
+      getState,
+      subscribe,
+      dispatch
+    }
+  };
+
+  function todos(state = [], action){
+      if(action.type === "ADD_TODO"){
+          return state.concat([action.todo])
+      }
+  };
+
+  const item = {
+    type: 'ADD_TODO',
+    todo: {
+      id: 0,
+      name: 'Learn Redux',
+      complete: false
+    }
+  };
+
+  const store = createStore(todos);
+
+  store.subscribe(() => {
+      console.log("The new state is :", store.getState())
+  })
+
+  store.dispatch({
+    type: 'ADD_TODO',
+    todo: {
+      id: 0,
+      name: 'Learn Redux',
+      complete: false
+    }
+  })
+
+  
